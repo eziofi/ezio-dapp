9f59d6a5266477d6c7760c3f8497a406917934f3
sunxiaofei <sunxiaofei@duoblock.cn>
2023-04-04 16:10:29 +0800

    代币总数修改

--- src/layouts/dashboard/header/TokenLIstItem.tsx
+++ src/layouts/dashboard/header/TokenLIstItem.tsx
@@ -93,11 +93,7 @@ export default function TokenLIstItem({ item }: { item: IOptions }) {
         <>
           {renderIcon(item)}
           <span style={{ fontWeight: '500' }}>
-            {item.balance ? (
-              '$' + formatString('' + parseFloat(item.balance) * parseFloat(price || '0'), 2).toString()
-            ) : (
-              <InlineSkeleton width={40} />
-            )}
+            {item.balance ? '$' + formatString(item.balance, 2) : <InlineSkeleton width={40} />}
           </span>
         </>
       </MyListItem>
