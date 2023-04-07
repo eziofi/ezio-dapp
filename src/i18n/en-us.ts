98ef395100ef5e39b504a1c0cd9f1304d73c0e4e
sunxiaofei <sunxiaofei@duoblock.cn>
2023-04-07 16:22:24 +0800

    删除 token_totalValue "$" 符号

--- src/i18n/en-us.ts
+++ src/i18n/en-us.ts
@@ -43,7 +43,7 @@ export default {
     EZAT_totalSupply: 'USDE Total Supply',
     // EZBT_totalSupply: ' Total Supply',
     EZIO_totalSupply: 'ezIO Total Supply',
-    token_totalValue: 'Token Total Value ($)',
+    token_totalValue: 'Token Total Value',
     EZAT_Rate: 'USDE Daily Rate',
     net: 'Network',
     lang: 'Language',
