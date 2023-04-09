a7bc3229839570e1abdbd839871a8153f104a7e0
sunxiaofei <sunxiaofei@duoblock.cn>
2023-04-09 11:25:24 +0800

    生产环境移除console

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const CracoLessPlugin = require('craco-less');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CompressionPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   plugins: [
@@ -28,6 +29,18 @@ module.exports = {
           process: 'process/browser',
         }),
         new CompressionPlugin(), // gzip压缩插件
+        new TerserPlugin({
+          terserOptions: {
+            ecma: undefined,
+            warnings: false,
+            parse: {},
+            compress: {
+              drop_console: process.env.NODE_ENV === 'production', // 生产环境下移除控制台所有的内容
+              drop_debugger: false, // 移除断点
+              pure_funcs: process.env.NODE_ENV === 'production' ? ['console.log'] : '', // 生产环境下移除console
+            },
+          },
+        }),
         // new BundleAnalyzerPlugin(),
       ],
     },
