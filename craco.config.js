27332b63678f697b13d4f87125111d3940210493
sunxiaofei <sunxiaofei@duoblock.cn>
2023-04-10 19:49:14 +0800

    修改移除console配置只在生产环境生效

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,8 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const CompressionPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   plugins: [
     {
@@ -23,26 +25,34 @@ module.exports = {
           url: require.resolve('url'),
         },
       },
-      plugins: [
-        new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-          process: 'process/browser',
-        }),
-        new CompressionPlugin(), // gzip压缩插件
-        // new TerserPlugin({
-        //   terserOptions: {
-        //     ecma: undefined,
-        //     warnings: false,
-        //     parse: {},
-        //     compress: {
-        //       drop_console: process.env.NODE_ENV === 'production', // 生产环境下移除控制台所有的内容
-        //       drop_debugger: false, // 移除断点
-        //       pure_funcs: process.env.NODE_ENV === 'production' ? ['console.log'] : '', // 生产环境下移除console
-        //     },
-        //   },
-        // }),
-        // new BundleAnalyzerPlugin(),
-      ],
+
+      plugins: isProduction
+        ? [
+            new webpack.ProvidePlugin({
+              Buffer: ['buffer', 'Buffer'],
+              process: 'process/browser',
+            }),
+            new CompressionPlugin(), // gzip压缩插件
+            new TerserPlugin({
+              terserOptions: {
+                ecma: 5,
+                warnings: false,
+                parse: {},
+                compress: {
+                  drop_console: true, // 生产环境下移除控制台所有的内容
+                  drop_debugger: false, // 移除断点
+                  pure_funcs: ['console.log'], // 生产环境下移除console
+                },
+              },
+            }),
+          ]
+        : [
+            new webpack.ProvidePlugin({
+              Buffer: ['buffer', 'Buffer'],
+              process: 'process/browser',
+            }),
+            new CompressionPlugin(), // gzip压缩插件
+          ],
     },
   },
 };
